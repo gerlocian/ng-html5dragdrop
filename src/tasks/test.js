4d9cc2ee1f3031f113ad468062c875ac5eb04390
gerlocian <pogithub@gmail.com>
2016-02-05 00:52:22 -0500

    Added phantomjs

--- src/tasks/test.js
+++ src/tasks/test.js
@@ -7,6 +7,7 @@
     function task(done) {
         var options = {};
         options.configFile = path.resolve('./karma.conf.js');
+        options.browsers = ['PhantomJS'];
         options.singleRun = true;
         new karma(options, done).start();
     }
