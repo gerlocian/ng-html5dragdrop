7e69e539bbf9cc6831e319534c2e89c5c32fb437
Patrick Ortiz <portiz@lulu.com>
2016-09-22 10:00:10 -0400

    Fixed issue with $log

--- dist/ng-html5dragdrop.js
+++ dist/ng-html5dragdrop.js
@@ -56,7 +56,7 @@
      *         draggable element is released. This method will fire whether or
      *         not the element was dragged and released into a valid drop zone.
      */
-    angular.module('html5DragDrop').directive('html5Drag', function ($log) {
+    angular.module('html5DragDrop').directive('html5Drag', ['$log', function ($log) {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
@@ -109,7 +109,7 @@
                 });
             }
         };
-    });
+    }]);
 
     /**
      * Generates a unique id for the element in the event that the element does
